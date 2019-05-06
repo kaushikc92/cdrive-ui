deea42fdeba60fec247305417e63a4261da92c62
Kaushik Chandrasekhar <kaushikc92@gmail.com>
2019-05-06 21:49:55 +0000

    Fetching token correctly

--- app/containers/App/baseUrl.js
+++ app/containers/App/baseUrl.js
@@ -1,4 +1,4 @@
-const cdriveUrl = "http://a406803666ed311e98ea412ac368fc7a-1956448726.us-east-1.elb.amazonaws.com/";
+const cdriveUrl = "http://a7648f6f5702911e98ea412ac368fc7a-1169430973.us-east-1.elb.amazonaws.com/";
 const authUrl = "http://a250afd7c6eba11e98ea412ac368fc7a-312971903.us-east-1.elb.amazonaws.com/";
 
 export {authUrl, cdriveUrl};
