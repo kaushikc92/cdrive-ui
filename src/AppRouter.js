8cb6c715631f2430cfb5ea7fa854311b71cf7962
Kaushik Chandrasekhar <kaushikc92@gmail.com>
2019-05-17 00:55:43 +0000

    Create React setup

---
+++ src/AppRouter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import App from './App';
+
+class AppRouter extends React.Component {
+    render() {
+        return(
+            <Router>
+                <Switch>
+                    <Route path="/home/" component={App} />
+                    <Redirect from='*' to='/home/' />
+                </Switch>
+            </Router>
+        );
+    }
+}
+
+export default AppRouter;
